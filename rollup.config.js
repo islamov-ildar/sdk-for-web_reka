7924c0d36e84978cf9fd420c021adce8b734386c
Islamov Ildar <islamov.ildar@gmail.com>
2022-05-23 14:20:35 +0300

    23-05-2022 updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,32 @@
 import pkg from "./package.json";
 import typescript from "@rollup/plugin-typescript";
+const iife = "dist/iife/sdk.js";
 
 export default {
-    external: Object.keys(pkg.dependencies),
-    input: "src/sdk.ts",
-    plugins: [typescript()],
-    output: [
-        {
-            format: "cjs",
-            file: pkg.main,
-            esModule: false,
-            sourcemap: true,
-        },
-        {
-            format: "es",
-            file: pkg.module,
-            sourcemap: true,
-        },
-        {
-            format: "iife",
-            file: pkg.jsdelivr,
-            name: "window",
-            extend: true,
-            globals: {
-                "cross-fetch": "window",
-                "FormData": "FormData",
-            },
-        },
-    ],
+  external: Object.keys(pkg.dependencies),
+  input: "src/sdk.ts",
+  plugins: [typescript()],
+  output: [
+    {
+      format: "cjs",
+      file: pkg.main,
+      esModule: false,
+      sourcemap: true,
+    },
+    {
+      format: "es",
+      file: pkg.module,
+      sourcemap: true,
+    },
+    {
+      format: "iife",
+      file: iife,
+      name: "window",
+      extend: true,
+      globals: {
+        "cross-fetch": "window",
+        FormData: "FormData",
+      },
+    },
+  ],
 };
